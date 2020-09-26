da3e860d61f49116aeb59a5b51d5770a2c50335e
d3c6e1 <dmytro.koval@nure.ua>
2020-09-26 13:24:33 +0300

    shaders, texture, encoding to UTH-8

--- DEngine/main.cpp
+++ DEngine/main.cpp
@@ -1,42 +1,27 @@
 #include <glad/glad.h> 
 #include <GLFW/glfw3.h>
 
+#include "shader.h"
+#include "stb_image.h"
+
 #include <iostream>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
 
-// Константы
+// РљРѕРЅСЃС‚Р°РЅС‚С‹
 const unsigned int SCR_WIDTH = 1200;
 const unsigned int SCR_HEIGHT = 900;
- 
-const char* vertexShaderSource = "#version 460 core\n"
-    "layout (location = 0) in vec3 aPos;\n"
-    "layout (location = 1) in vec3 aColor;\n"
-    "out vec3 ourColor;\n"
-    "void main()\n"
-    "{\n"
-    "   gl_Position = vec4(aPos, 1.0);\n"
-    "   ourColor = aColor;\n"
-    "}\0";
-
-const char* fragmentShaderSource = "#version 460 core\n"
-   "out vec4 FragColor;\n"
-    "in vec3 ourColor;\n"
-    "void main()\n"
-    "{\n"
-    "   FragColor = vec4(ourColor, 1.0f);\n"
-    "}\n\0";
 
 int main()
 {
-	// glfw: инициализация и конфигурирование
+	// glfw: РёРЅРёС†РёР°Р»РёР·Р°С†РёСЏ Рё РєРѕРЅС„РёРіСѓСЂРёСЂРѕРІР°РЅРёРµ
 	glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	
-	// glfw: создание окна
+	// glfw: СЃРѕР·РґР°РЅРёРµ РѕРєРЅР°
     GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "DEngine", NULL, NULL);
     if (window == NULL)
     {
@@ -47,136 +32,126 @@ int main()
     glfwMakeContextCurrent(window);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
  
-    // glad: загрузка всех указателей на OpenGL-функции
+    // glad: Р·Р°РіСЂСѓР·РєР° РІСЃРµС… СѓРєР°Р·Р°С‚РµР»РµР№ РЅР° OpenGL-С„СѓРЅРєС†РёРё
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
 
-    // Компилирование нашей шейдерной программы
- 
-    // Вершинный шейдер
-    int vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-    glCompileShader(vertexShader);
- 
-    // Проверка на наличие ошибок компилирования вершинного шейдера
-    int success;
-    char infoLog[512];
-    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-    if (!success)
-    {
-        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
-    }
- 
-    // Фрагментный шейдер
-    int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-    glCompileShader(fragmentShader);
- 
-    // Проверка на наличие ошибок компилирования фрагментного шейдера
-    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
-    if (!success)
-    {
-        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
-    }
- 
-    // Связывание шейдеров
-    int shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram, vertexShader);
-    glAttachShader(shaderProgram, fragmentShader);
-    glLinkProgram(shaderProgram);
- 
-    // Проверка на наличие ошибок компилирования связывания шейдеров
-    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-    if (!success) {
-        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
-    }
-    glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShader);
- 
-    // Указывание вершин (и буферов) и настройка вершинных атрибутов
+    // РљРѕРјРїРёР»РёСЂРѕРІР°РЅРёРµ РЅР°С€РµР№ С€РµР№РґРµСЂРЅРѕР№ РїСЂРѕРіСЂР°РјРјС‹
+
+	Shader ourShader("shaders/texture.vs", "shaders/texture.fs");
+
+    // РЈРєР°Р·С‹РІР°РЅРёРµ РІРµСЂС€РёРЅ (Рё Р±СѓС„РµСЂРѕРІ) Рё РЅР°СЃС‚СЂРѕР№РєР° РІРµСЂС€РёРЅРЅС‹С… Р°С‚СЂРёР±СѓС‚РѕРІ
     float vertices[] = {
-        // координаты         // цвета
-         1.0f, -1.0f, 0.0f,  0.07f, 0.07f, 0.84f,  // нижняя правая вершина
-        -1.0f, -1.0f, 0.0f,  0.92f, 0.07f, 0.15f,  // нижняя левая вершина
-         0.0f,  1.0f, 0.0f,  0.46f, 0.20f, 0.63f   // верхняя вершина 
+	    // РєРѕРѕСЂРґРёРЅР°С‚С‹          // С†РІРµС‚Р°           // С‚РµРєСЃС‚СѓСЂРЅС‹Рµ РєРѕРѕСЂРґРёРЅР°С‚С‹
+	     0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // РІРµСЂС…РЅСЏСЏ РїСЂР°РІР°СЏ
+	     0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // РЅРёР¶РЅСЏСЏ РїСЂР°РІР°СЏ
+	    -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // РЅРёР¶РЅСЏСЏ Р»РµРІР°СЏ
+	    -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // РІРµСЂС…РЅСЏСЏ Р»РµРІР°СЏ 
     };
- 
-    unsigned int VBO, VAO;
+
+    unsigned int indices[] = {
+        0, 1, 3, // РїРµСЂРІС‹Р№ С‚СЂРµСѓРіРѕР»СЊРЅРёРє
+        1, 2, 3  // РІС‚РѕСЂРѕР№ С‚СЂРµСѓРіРѕР»СЊРЅРёРє
+    };
+
+    unsigned int VBO, VAO, EBO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
- 	
-    // Сначала связываем объект вершинного массива, затем связываем и устанавливаем вершинный буфер(ы), и затем конфигурируем вершинный атрибут(ы)    
-	glBindVertexArray(VAO);
+    glGenBuffers(1, &EBO);
+
+    glBindVertexArray(VAO);
+
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
- 
-    // Координатные атрибуты
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    // РєРѕРѕСЂРґРёРЅР°С‚РЅС‹Рµ Р°С‚СЂРёР±СѓС‚С‹
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
-	
-    // Цветовые атрибуты
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    // С†РІРµС‚РѕРІС‹Рµ Р°С‚СЂРёР±СѓС‚С‹
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
- 
-    // Вы можете отменить привязку VАО после этого, чтобы другие вызовы VАО случайно не изменили этот VAO (но подобное довольно редко случается)
-    // Модификация других VAO требует вызов glBindVertexArray(), поэтому мы обычно не снимаем привязку VAO (или VBO), когда это не требуется напрямую   
-	// glBindVertexArray(0);
- 
-    // Поскольку у нас есть только один шейдер, мы также можем просто активировать наш шейдер заранее, если нужно
-    glUseProgram(shaderProgram);
- 
- 
-    // Раскомментируйте строчку ниже для отрисовки полигонов в режиме каркаса
-    // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    // Р°С‚СЂРёР±СѓС‚С‹ С‚РµРєСЃС‚СѓСЂРЅС‹С… РєРѕРѕСЂРґРёРЅР°С‚
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
+
 	
-	glBindVertexArray(VAO);
+    // Р·Р°РіСЂСѓР·РєР° Рё СЃРѕР·РґР°РЅРёРµ С‚РµРєСЃС‚СѓСЂС‹
+    // -------------------------
+    unsigned int texture;
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture); // РІСЃРµ РїРѕСЃР»РµРґСѓСЋС‰РёРµ GL_TEXTURE_2D-РѕРїРµСЂР°С†РёРё С‚РµРїРµСЂСЊ Р±СѓРґСѓС‚ РІР»РёСЏС‚СЊ РЅР° РґР°РЅРЅС‹Р№ С‚РµРєСЃС‚СѓСЂРЅС‹Р№ РѕР±СЉРµРєС‚
+    // СѓСЃС‚Р°РЅРѕРІРєР° РїР°СЂР°РјРµС‚СЂРѕРІ РЅР°Р»РѕР¶РµРЅРёСЏ С‚РµРєСЃС‚СѓСЂС‹
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	// СѓСЃС‚Р°РЅРѕРІРєР° РјРµС‚РѕРґР° РЅР°Р»РѕР¶РµРЅРёСЏ С‚РµРєСЃС‚СѓСЂС‹ GL_REPEAT (СЃС‚Р°РЅРґР°СЂС‚РЅС‹Р№ РјРµС‚РѕРґ РЅР°Р»РѕР¶РµРЅРёСЏ)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    // СѓСЃС‚Р°РЅРѕРІРєР° РїР°СЂР°РјРµС‚СЂРѕРІ С„РёР»СЊС‚СЂР°С†РёРё С‚РµРєСЃС‚СѓСЂС‹
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    // Р·Р°РіСЂСѓР·РєР° РёР·РѕР±СЂР°Р¶РµРЅРёСЏ, СЃРѕР·РґР°РЅРёРµ С‚РµРєСЃС‚СѓСЂС‹ Рё РіРµРЅРµСЂРёСЂРѕРІР°РЅРёРµ mipmap-СѓСЂРѕРІРЅРµР№
+    int width, height, nrChannels;
+    unsigned char* data = stbi_load("textures/marble", &width, &height, &nrChannels, 0);
+    if (data)
+    {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else
+    {
+        std::cout << "Failed to load texture" << std::endl;
+    }
+    stbi_image_free(data);
+
 	
-    // Цикл рендеринга
+    // Р¦РёРєР» СЂРµРЅРґРµСЂРёРЅРіР°
     while (!glfwWindowShouldClose(window))
     {
-        // Обработка ввода
+        // РћР±СЂР°Р±РѕС‚РєР° РІРІРѕРґР°
         processInput(window);
 		
-		// Выполнение рендеринга
+		// Р’С‹РїРѕР»РЅРµРЅРёРµ СЂРµРЅРґРµСЂРёРЅРіР°
         glClearColor(0.47f, 0.48f, 0.49f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
     	
-    	// Рендеринг треугольника
+        // РЎРІСЏР·С‹РІР°РЅРёРµ С‚РµРєСЃС‚СѓСЂС‹
+        glBindTexture(GL_TEXTURE_2D, texture);
+
+        // Р РµРЅРґРµСЂРёРЅРі СЏС‰РёРєР°
+        ourShader.use();
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
  
-        // glfw: обмен содержимым front- и back- буферов. Отслеживание событий Ввода/Ввывода (была ли нажата/отпущена кнопка, перемещен курсор мыши и т.п.)
+        // glfw: РѕР±РјРµРЅ СЃРѕРґРµСЂР¶РёРјС‹Рј front- Рё back- Р±СѓС„РµСЂРѕРІ. РћС‚СЃР»РµР¶РёРІР°РЅРёРµ СЃРѕР±С‹С‚РёР№ Р’РІРѕРґР°/Р’РІС‹РІРѕРґР° (Р±С‹Р»Р° Р»Рё РЅР°Р¶Р°С‚Р°/РѕС‚РїСѓС‰РµРЅР° РєРЅРѕРїРєР°, РїРµСЂРµРјРµС‰РµРЅ РєСѓСЂСЃРѕСЂ РјС‹С€Рё Рё С‚.Рї.)
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
 	
-	// Опционально: освобождаем все ресурсы, как только они выполнили своё предназначение
-    glDeleteVertexArrays(1, &VAO);
+	// РћРїС†РёРѕРЅР°Р»СЊРЅРѕ: РѕСЃРІРѕР±РѕР¶РґР°РµРј РІСЃРµ СЂРµСЃСѓСЂСЃС‹, РєР°Рє С‚РѕР»СЊРєРѕ РѕРЅРё РІС‹РїРѕР»РЅРёР»Рё СЃРІРѕС‘ РїСЂРµРґРЅР°Р·РЅР°С‡РµРЅРёРµ
+	glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
  
-    // glfw: завершение, освобождение всех ранее задействованных GLFW-ресурсов
+    // glfw: Р·Р°РІРµСЂС€РµРЅРёРµ, РѕСЃРІРѕР±РѕР¶РґРµРЅРёРµ РІСЃРµС… СЂР°РЅРµРµ Р·Р°РґРµР№СЃС‚РІРѕРІР°РЅРЅС‹С… GLFW-СЂРµСЃСѓСЂСЃРѕРІ
     glfwTerminate();
     return 0;
 }
  
-// Обработка всех событий ввода: запрос GLFW о нажатии/отпускании клавиш на клавиатуре в данном кадре и соответствующая обработка данных событий
+// РћР±СЂР°Р±РѕС‚РєР° РІСЃРµС… СЃРѕР±С‹С‚РёР№ РІРІРѕРґР°: Р·Р°РїСЂРѕСЃ GLFW Рѕ РЅР°Р¶Р°С‚РёРё/РѕС‚РїСѓСЃРєР°РЅРёРё РєР»Р°РІРёС€ РЅР° РєР»Р°РІРёР°С‚СѓСЂРµ РІ РґР°РЅРЅРѕРј РєР°РґСЂРµ Рё СЃРѕРѕС‚РІРµС‚СЃС‚РІСѓСЋС‰Р°СЏ РѕР±СЂР°Р±РѕС‚РєР° РґР°РЅРЅС‹С… СЃРѕР±С‹С‚РёР№
 void processInput(GLFWwindow *window)
 {
     if(glfwGetKey(window, GLFW_KEY_K) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
 }
  
-// glfw: всякий раз, когда изменяются размеры окна (пользователем или оперионной системой), вызывается данная callback-функция
+// glfw: РІСЃСЏРєРёР№ СЂР°Р·, РєРѕРіРґР° РёР·РјРµРЅСЏСЋС‚СЃСЏ СЂР°Р·РјРµСЂС‹ РѕРєРЅР° (РїРѕР»СЊР·РѕРІР°С‚РµР»РµРј РёР»Рё РѕРїРµСЂРёРѕРЅРЅРѕР№ СЃРёСЃС‚РµРјРѕР№), РІС‹Р·С‹РІР°РµС‚СЃСЏ РґР°РЅРЅР°СЏ callback-С„СѓРЅРєС†РёСЏ
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
-    // Убеждаемся, что окно просмотра соответствует новым размерам окна 
-	// Обратите внимание, что ширина и высота будут значительно больше, чем указано на Retina-дисплеях
+    // РЈР±РµР¶РґР°РµРјСЃСЏ, С‡С‚Рѕ РѕРєРЅРѕ РїСЂРѕСЃРјРѕС‚СЂР° СЃРѕРѕС‚РІРµС‚СЃС‚РІСѓРµС‚ РЅРѕРІС‹Рј СЂР°Р·РјРµСЂР°Рј РѕРєРЅР° 
+	// РћР±СЂР°С‚РёС‚Рµ РІРЅРёРјР°РЅРёРµ, С‡С‚Рѕ С€РёСЂРёРЅР° Рё РІС‹СЃРѕС‚Р° Р±СѓРґСѓС‚ Р·РЅР°С‡РёС‚РµР»СЊРЅРѕ Р±РѕР»СЊС€Рµ, С‡РµРј СѓРєР°Р·Р°РЅРѕ РЅР° Retina-РґРёСЃРїР»РµСЏС…
     glViewport(0, 0, width, height);
 }
--- DEngine/shader.h
+++ DEngine/shader.h
@@ -13,34 +13,34 @@ class Shader
 public:
     unsigned int ID;
 	
-    // Конструктор генерирует шейдер на лету
+    // РљРѕРЅСЃС‚СЂСѓРєС‚РѕСЂ РіРµРЅРµСЂРёСЂСѓРµС‚ С€РµР№РґРµСЂ РЅР° Р»РµС‚Сѓ
     Shader(const char* vertexPath, const char* fragmentPath)
     {
-        // Этап №1: Получение исходного кода вершинного/фрагментного шейдера из переменной filePath
+        // Р­С‚Р°Рї в„–1: РџРѕР»СѓС‡РµРЅРёРµ РёСЃС…РѕРґРЅРѕРіРѕ РєРѕРґР° РІРµСЂС€РёРЅРЅРѕРіРѕ/С„СЂР°РіРјРµРЅС‚РЅРѕРіРѕ С€РµР№РґРµСЂР° РёР· РїРµСЂРµРјРµРЅРЅРѕР№ filePath
         std::string vertexCode;
         std::string fragmentCode;
         std::ifstream vShaderFile;
         std::ifstream fShaderFile;
 		
-        // Убеждаемся, что объекты ifstream могут выбросить исключение
+        // РЈР±РµР¶РґР°РµРјСЃСЏ, С‡С‚Рѕ РѕР±СЉРµРєС‚С‹ ifstream РјРѕРіСѓС‚ РІС‹Р±СЂРѕСЃРёС‚СЊ РёСЃРєР»СЋС‡РµРЅРёРµ
         vShaderFile.exceptions (std::ifstream::failbit | std::ifstream::badbit);
         fShaderFile.exceptions (std::ifstream::failbit | std::ifstream::badbit);
         try 
         {
-            // Открываем файлы
+            // РћС‚РєСЂС‹РІР°РµРј С„Р°Р№Р»С‹
             vShaderFile.open(vertexPath);
             fShaderFile.open(fragmentPath);
             std::stringstream vShaderStream, fShaderStream;
 			
-            // Считываем содержимое файловых буферов в потоки 
+            // РЎС‡РёС‚С‹РІР°РµРј СЃРѕРґРµСЂР¶РёРјРѕРµ С„Р°Р№Р»РѕРІС‹С… Р±СѓС„РµСЂРѕРІ РІ РїРѕС‚РѕРєРё 
             vShaderStream << vShaderFile.rdbuf();
             fShaderStream << fShaderFile.rdbuf();
 			
-            // Закрываем файлы
+            // Р—Р°РєСЂС‹РІР°РµРј С„Р°Р№Р»С‹
             vShaderFile.close();
             fShaderFile.close();
 			
-            // Конвертируем данные из потока в строковые переменные
+            // РљРѕРЅРІРµСЂС‚РёСЂСѓРµРј РґР°РЅРЅС‹Рµ РёР· РїРѕС‚РѕРєР° РІ СЃС‚СЂРѕРєРѕРІС‹Рµ РїРµСЂРµРјРµРЅРЅС‹Рµ
             vertexCode   = vShaderStream.str();
             fragmentCode = fShaderStream.str();
         }
@@ -51,40 +51,40 @@ public:
         const char* vShaderCode = vertexCode.c_str();
         const char * fShaderCode = fragmentCode.c_str();
 
-        // Этап №2: Компилируем шейдеры
+        // Р­С‚Р°Рї в„–2: РљРѕРјРїРёР»РёСЂСѓРµРј С€РµР№РґРµСЂС‹
         unsigned int vertex, fragment;
 
-        // Вершинный шейдер
+        // Р’РµСЂС€РёРЅРЅС‹Р№ С€РµР№РґРµСЂ
         vertex = glCreateShader(GL_VERTEX_SHADER);
         glShaderSource(vertex, 1, &vShaderCode, NULL);
         glCompileShader(vertex);
         checkCompileErrors(vertex, "VERTEX");
 		
-        // Фрагментный шейдер
+        // Р¤СЂР°РіРјРµРЅС‚РЅС‹Р№ С€РµР№РґРµСЂ
         fragment = glCreateShader(GL_FRAGMENT_SHADER);
         glShaderSource(fragment, 1, &fShaderCode, NULL);
         glCompileShader(fragment);
         checkCompileErrors(fragment, "FRAGMENT");
 		
-        // Шейдерная программа
+        // РЁРµР№РґРµСЂРЅР°СЏ РїСЂРѕРіСЂР°РјРјР°
         ID = glCreateProgram();
         glAttachShader(ID, vertex);
         glAttachShader(ID, fragment);
         glLinkProgram(ID);
         checkCompileErrors(ID, "PROGRAM");
 
-        // После того, как мы связали шейдеры с нашей программой, удаляем их, так как они нам больше не нужны
+        // РџРѕСЃР»Рµ С‚РѕРіРѕ, РєР°Рє РјС‹ СЃРІСЏР·Р°Р»Рё С€РµР№РґРµСЂС‹ СЃ РЅР°С€РµР№ РїСЂРѕРіСЂР°РјРјРѕР№, СѓРґР°Р»СЏРµРј РёС…, С‚Р°Рє РєР°Рє РѕРЅРё РЅР°Рј Р±РѕР»СЊС€Рµ РЅРµ РЅСѓР¶РЅС‹
         glDeleteShader(vertex);
         glDeleteShader(fragment);
     }
 
-    // Активация шейдера
+    // РђРєС‚РёРІР°С†РёСЏ С€РµР№РґРµСЂР°
     void use() 
     { 
         glUseProgram(ID); 
     }
 
-    // Полезные uniform-функции
+    // РџРѕР»РµР·РЅС‹Рµ uniform-С„СѓРЅРєС†РёРё
     void setBool(const std::string &name, bool value) const
     {         
         glUniform1i(glGetUniformLocation(ID, name.c_str()), (int)value); 
@@ -99,7 +99,7 @@ public:
     }
 
 private:
-    // Полезные функции для проверки ошибок компиляции/связывания шейдеров
+    // РџРѕР»РµР·РЅС‹Рµ С„СѓРЅРєС†РёРё РґР»СЏ РїСЂРѕРІРµСЂРєРё РѕС€РёР±РѕРє РєРѕРјРїРёР»СЏС†РёРё/СЃРІСЏР·С‹РІР°РЅРёСЏ С€РµР№РґРµСЂРѕРІ
     void checkCompileErrors(unsigned int shader, std::string type)
     {
         int success;
